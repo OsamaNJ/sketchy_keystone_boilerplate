dde5c2487acbf96bdbfc6197c56032d63c48070c
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-05-24 04:48:09 +0200

    Initial commit

---
+++ models/Account.js
@@ -0,0 +1,34 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Account Model
+ * ==========
+ */
+var Account = new keystone.List('Account');
+
+Account.add({
+	name: { type: Types.Name, required: true, index: true },
+	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
+	password: { type: Types.Password, initial: true, required: true },
+}, 'Permissions', {
+	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+});
+
+// Provide access to Keystone
+Account.schema.virtual('canAccessKeystone').get(function () {
+	return this.isAdmin;
+});
+
+
+/**
+ * Relationships
+ */
+Account.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
+
+
+/**
+ * Registration
+ */
+Account.defaultColumns = 'name, email, isAdmin';
+Account.register();
